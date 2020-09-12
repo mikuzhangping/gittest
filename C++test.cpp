4f45811a9367f7a6f2e7c585b36197c11e5bc8f4
miku <173312858@qq.com>
2020-09-12 13:49:12 +0800

    add c++ test 没有配置c++环境

---
+++ C++test.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string.h>
+#include<cstring>
+using namespace std;
+void input()
+{
+
+	
+}
+
+int main(int argc, char *argv[]) {
+	cout << endl;
+	cout << argc << endl;
+	for (int i = 0;i<argc;i++) {
+		
+		cout << argv[i]<<" ";
+	}
+	return 0;
+}
